dee4094cffdf83dd78d12474e9d0a0daa3fc1448
Joe Flateau <joe@joeflateau.net>
2019-06-15 09:44:29 -0400

    fix demo

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 // Import stylesheets
-import './style.scss';
+import "./style.scss";
 
-import { SwipeAwaySheet, listen } from "./sheet";
+import { SwipeAwaySheet } from "./sheet";
+import { listen } from "./listen";
 
 const logWindow = document.querySelector(".output");
+
 function log(message: string) {
   logWindow.innerHTML = message;
 }
-
 
 const _open = document.querySelector(".open") as HTMLElement;
 const _sheet = document.querySelector(".sheet") as HTMLElement;
@@ -16,13 +17,19 @@ const _backdrop = document.querySelector(".sheet-backdrop") as HTMLElement;
 const _container = document.querySelector(".sheet-container") as HTMLElement;
 const _cancel = document.querySelector(".sheet-cancel") as HTMLElement;
 
-const gesture = new SwipeAwaySheet(_container, _sheet, _sheet_content,_backdrop, [270]);
+const gesture = new SwipeAwaySheet(
+  _container,
+  _sheet,
+  _sheet_content,
+  _backdrop,
+  [270]
+);
 
 gesture.open();
 
 listen(_open, ["click"], () => {
   gesture.open();
-})
+});
 listen(_cancel, ["click"], () => {
   gesture.close();
-})
+});
